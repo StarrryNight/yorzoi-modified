1228bd54a37a09946fea25e16c9319a287b795cd
Samuel Lau <samuel06@login3.int.fir.alliancecan.ca>
2026-01-22 15:32:34 -0800

    add process_data.py to split data into RNA-swq profiles

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 import pickle 
 import pandas as pd
-c = pd.read_pickle("samples.pkl")
+c = pd.read_pickle("categorized/759h10_NC_000004.12.pkl")
 print(c.columns)
-print(c.iloc[0])
+print(c.iloc[:])
---
+++ process_data.py
@@ -0,0 +1,20 @@
+
+import pickle 
+import pandas as pd
+
+
+dic = {}
+c = pd.read_pickle("samples.pkl")
+for index, row in c.iterrows():
+    if row['chr'] not in dic:
+        dic.update({row['chr']: c.loc[[index]]})
+    else:
+        temp = pd.concat([dic.get(row['chr']), c.loc[[index]]], ignore_index=True)
+        dic.update({row['chr']: temp})
+file_path = "categorized"
+for key,value in dic.items():
+    with open(f"{file_path}/{key}.pkl", 'wb') as file:
+        pickle.dump(value, file)
+
+
+
