f6ef0bdbb22b0b6188b5853a7d153caf31b95098
Samuel Lau <samuel06@fc10514.int.fir.alliancecan.ca>
2026-01-23 11:44:49 -0800

    change yeast test to test different profiles separately

--- test_yeast.py
+++ test_yeast.py
@@ -7,28 +7,27 @@ from yorzoi.train_utils.data import create_datasets, create_dataloaders
 from yorzoi.config import TrainConfig, BorzoiConfig
 from yorzoi.train import test_model
 import pandas as pd
-
+import json
 
 profiles = pd.read_csv("category_names.txt")
 confi = TrainConfig.read_from_json("train_config.json")
 
-# Iterate through category names and predict each 
-for index,row in profiles.itertuples():
-    confi.path_to_samples = row[1]
-train, val, test = create_datasets(confi)
-train_d, val_d, test_d = create_dataloaders(confi, train, val, test)
+# Set up model
 md = Borzoi(BorzoiConfig.read_from_json(confi.borzoi_cfg))
-
 model_sd = torch.load("trained_model/model_best.pth")
 md.load_state_dict(model_sd)
 md = md.to("cuda:0")
 md.eval()
-test_model(
-    base_folder="evaluations" ,test_loader=test_d,model=md,criterion=None,device="cuda:0")
-import json
 
-# Load track names from annotation file 
-with open("track_annotation.json", "r") as f:
-    track_names = json.load(f)
 
-idx2name = track_names['+'] + track_names['-']
+# Iterate through category names and predict each 
+for index,row in profiles.itertuples():
+    confi.path_to_samples = row["path"]
+    train, val, test = create_datasets(confi)
+    train_d, val_d, test_d = create_dataloaders(confi, train, val, test)
+    test_model(
+        base_folder=f"evaluations/{row["path"]}" ,test_loader=test_d,model=md,criterion=None,device="cuda:0")
+    break
+
+
+
