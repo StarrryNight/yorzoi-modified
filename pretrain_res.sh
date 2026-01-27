e1a096cd55c3569f617f73ecc1f1aea82a1f9296
StarrryNight <lausingsamuel@gmail.com>
2026-01-27 13:06:07 -0800

    training

---
+++ pretrain_res.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=yorzoi_pretrain
+#SBATCH --account=rrg-cdeboer
+#SBATCH --time=24:00:00         
+#SBATCH --gpus=h100:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=0
+#SBATCH --mem=32GB
+#SBATCH --output=results/test_outputs/%A_%a.out
+#SBATCH --error=results/training_log/%A_%a.err
+#SBATCH --profile=none
+
+python3 pretrain_res.py
